df29c9b02f67a8a4f2bdf9ef48034b53c62ea03a
korbi <korbinian90@gmail.com>
2020-04-14 13:04:41 +0200

    add single echo option (some bugs)

--- test/functions_test.jl
+++ test/functions_test.jl
@@ -12,4 +12,46 @@ data = Data(mag_nii, phase_nii, hdr, TEs)
 
 swi = calculateSWI(data)
 
-printstyled("Tests Passed!"; color=:green)
+options = [
+    Options()
+    Options(;σ=[6,6,3])
+    Options(;unwrapping=:romeo)
+    Options(;unwrapping=:laplacianslice)
+    Options(;mode=:positive)
+    Options(;mode=:negative)
+    Options(;mode=:triangular)
+    Options(;level=6)
+    Options(;level=200)
+    Options(;combination_type=:CNR=>(:wm, :gm))
+    Options(;combination_type=:CNR=>(:wm, :ven))
+    Options(;combination_type=:CNR=>(:wm, :csf))
+    Options(;combination_type=:CNR=>(:ms, :iron_ring))
+    Options(;combination_type=:SE)
+    Options(;combination_type=:SE=>10)
+    Options(;combination_type=:SE=>5)
+]
+# TODO
+wrong_options = [
+    Options(;level=-5)
+    Options(;level=0)
+    Options(;combination_type=:SE=>0)
+    Options(;combination_type=:SE=>500)
+]
+
+s = []
+for o in options
+    try
+        @time push!(s, calculateSWI(data, o)[:,:,20])
+    catch
+        @show o
+        Base.invokelatest(Base.display_error, Base.catch_stack())
+    end
+end
+
+# all results should be different
+for i in 1:length(s), j in 1:(i-1)
+    @test s[i] != s[j]
+    if s[i] == s[j]
+        @show (i,j)
+    end
+end
