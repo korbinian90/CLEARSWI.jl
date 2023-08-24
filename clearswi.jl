3b387774b3941f312299460ceafaeb983c348b76
Korbinian Eckstein <korbinian.eckstein@gmail.com>
2023-08-24 11:40:15 +1000

    fix cleaswi.jl command line runner; now automatically installs dependencies if missing

--- clearswi.jl
+++ clearswi.jl
@@ -1,6 +1,11 @@
 import Pkg
 Pkg.activate(@__DIR__)
-using CLEARSWI, ArgParse
+try
+    using CLEARSWI, ArgParse
+catch
+    Pkg.add(["CLEARSWI", "ArgParse"])
+    using CLEARSWI, ArgParse
+end
 
 @time msg = clearswi_main(ARGS)
 println(msg)
